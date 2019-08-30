2ca70df5fb7cd0969c76416bf001feccc67ad929
github username <github email address>
2019-08-30 10:47:23 -0400

    finished deliverables

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,32 @@
+#driver has many rides
+class Driver
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def rides 
+        Ride.all.select {|ride| ride.driver == self}
+    end
+
+    def passenger_names
+        passengers = rides.map {|ride|ride.passenger.name}
+        passengers.uniq {|name|name}
+    end
+
+    def driver_distance
+        rides.reduce(0) {|memo, ride| memo + ride.distance}
+    end
+
+    def self.mileage_cap(distance)
+        self.all.select {|driver| driver.driver_distance > distance}
+    end
+end
 
--- tools/console.rb
+++ tools/console.rb
@@ -4,7 +4,20 @@ def reload
   load 'config/environment.rb'
 end
 
+passenger1 = Passenger.new("Tom")
+passenger2 = Passenger.new("Toney")
+passenger3 = Passenger.new("Jake")
 
-# Put your variables here~!
+driver1 = Driver.new("Tim")
+driver2 = Driver.new("Britanny")
+driver3 = Driver.new("Foo")
+
+ride1 = Ride.new(driver1, passenger1, 100.2)
+ride2 = Ride.new(driver2, passenger2, 50.2)
+ride3 = Ride.new(driver3, passenger3, 70.7)
+ride4 = Ride.new(driver1, passenger1, 80.9)
+ride5 = Ride.new(driver2, passenger2, 20.2)
+
+
 
 binding.pry
